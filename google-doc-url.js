f4b73ad4e9b9a0335418c5a7fbc99072f408834d
blackgirl-sia <35097416+blackgirl-sia@users.noreply.github.com>
2019-04-04 10:26:25 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1J4Z7NWniiDMzG0t_BNECm9LyhFeGIhSu9OVaOVYQeUs/edit#gid=0';
